4786b4a374714a5d5f3d83e34963b38612b60692
luthfiMeteor <xoneskyone@gmail.com>
2023-02-24 17:01:57 +0700

    fix error

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\Api\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 Route::apiResource('posts', PostController::class);
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
